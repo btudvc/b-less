38b26451ce1930cdce7c60efe5b680312ebe8c25
Batuhan Deveci <batuhandeveci.bd@gmail.com>
2026-06-02 14:07:23 +0300

    Configure Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 (function(root) {
   root.BLESS_FIREBASE_CONFIG = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyBPKjaKoQELG41uA5v41tR4QEe9rQ73IQQ',
+    authDomain: 'b-less.firebaseapp.com',
+    projectId: 'b-less',
+    storageBucket: 'b-less.firebasestorage.app',
+    messagingSenderId: '900456996740',
+    appId: '1:900456996740:web:180a101ace66c924b587d1',
+    measurementId: 'G-SWSL2DT297',
   };
 
   // Firebase Console -> Project settings -> Cloud Messaging -> Web Push certificates.
-  root.BLESS_FIREBASE_VAPID_KEY = '';
+  root.BLESS_FIREBASE_VAPID_KEY = 'BJAzRdI2X-92zO2rnUxGag6zQdVLws09scAV1-BPTU4ZQQMWljhDL0mCw7QtiO3KQp-y17nOVa9pgru1L_Aqimw';
 })(typeof self !== 'undefined' ? self : window);
